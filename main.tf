6e573cd52f31c19853e4a117e2e3128db0e991ce
saif25 <saif25@gmail.com>
2025-09-28 00:40:48 +0530

    Your descriptive commit message

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ module "networking" {
     source = "./networking"
     vpc_cidr = var.vpc_cidr
     vpc_name = var.vpc_name
+    cidr_public_subnet = var.cidr_public_subnet
+    ap_availability_zone = var.ap_availability_zone
+    cidr_private_subnet = var.cidr_private_subnet
+
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,5 @@
 variable "vpc_cidr" {}
 variable "vpc_name" {}
+variable "cidr_public_subnet" {}
+variable "ap_availability_zone" {}
+variable "cidr_private_subnet" {}
