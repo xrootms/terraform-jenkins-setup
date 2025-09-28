06cf6fcfe797ff5490640113aad2adc6aa12f428
saif25 <saif25@gmail.com>
2025-09-28 23:59:00 +0530

    added route tables

--- networking/main.tf
+++ networking/main.tf
@@ -39,3 +39,39 @@ resource "aws_internet_gateway" "dev_proj_1_ig" {
     tags = { Name = "dev_proj_1_ig" }
 
 }
+
+# public route_table
+
+resource "aws_route_table" "dev_proj_1_public_route_table" {
+    vpc_id = aws_vpc.dev_proj_1_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dev_proj_1_ig.id
+    }
+    tags = { Name = "dev_proj_1_public_rt" }
+}
+
+# Public Route_Table and Public Subnet Association
+
+resource "aws_route_table_association" "dev_proj_1_public_rt_subnet_association" {
+    count = length(aws_subnet.dev_proj_public_subnets)
+    subnet_id = aws_subnet.dev_proj_public_subnets[count.index].id
+    route_table_id = aws_route_table.dev_proj_1_public_route_table.id
+}
+
+# Private Route Table
+
+resource "aws_route_table" "dev_proj_1_private_route_table" {
+    vpc_id = aws_vpc.dev_proj_1_vpc.id
+     #depends_on = [aws_nat_gateway.nat_gateway]
+    tags = { Name = "dev_proj_1_private_rt" }
+}
+
+# Private Route_Table and private Subnet Association
+
+resource "aws_route_table_association" "dev_proj_1_private_rt_subnet_association" {
+    count = length(aws_subnet.dev_proj_1_private_subnets)
+    subnet_id = aws_subnet.dev_proj_1_private_subnets[count.index].id
+    route_table_id = aws_route_table.dev_proj_1_private_route_table.id
+
+}
