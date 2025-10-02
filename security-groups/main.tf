d6e9cbae4b297e8b9c4cf0b7c9ebb77c9a497cfa
saif25 <saif25@gmail.com>
2025-10-02 12:48:09 +0530

    modified SG/main.tf: used one ingress rules with count instead of three separate ingress

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -1,9 +1,7 @@
 variable "ec2_sg_name" {}
 variable "vpc_id" {}
 variable "ec2_jenkins_sg_name" {}
-variable "sg_ports" {
-  default = [22, 80, 443]  # List of ports you want to allow
-}
+variable "sg_ports" { default = [22, 80, 443] }
 
 output "sg_ec2_sg_ssh_http_id" {
   value = aws_security_group.ec2_sg_ssh_http_https.id
@@ -14,56 +12,40 @@ output "sg_ec2_jenkins_port_8080" {
 }
 
 
-
-# SG for 22, 80, 443
+#SG for ports: 22, 80, 443
 
 resource "aws_security_group" "ec2_sg_ssh_http_https" {
   name        = var.ec2_sg_name
-   #description = "Enable the Port 22(SSH) & Port 80(http)"
   vpc_id      = var.vpc_id
-    
-  ingress {
-    description = "Allow remote SSH from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    }
+  description = "Enable the Port 22(SSH), Port 80(http) & port 443(https)"
 
-  ingress {
-    description = "Allow HTTP request from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-
-  }
-
-  ingress {
-    description = "Allow HTTPS request from anywhere"
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-
-  }
-
+  #Allow all outbound traffic 
+  
   egress {
-    description = "Allow outgoing request"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    
+    description = "Allow outgoing request to anywhere"
   }
-  tags = { Name = "Security Groups to allow SSH(22) and HTTP(80)" }
+  tags = { Name = "Security Group: SSH(22), HTTP(80) and HTTPs(443)" }
+}
 
+#Ingress rules using count
+
+resource "aws_security_group_rule" "sg_ingress" {
+  count             = length(var.sg_ports)
+  type              = "ingress"
+  from_port         = var.sg_ports[count.index]
+  to_port           = var.sg_ports[count.index]
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg_ssh_http_https.id
+  description       = "Allow port ${var.sg_ports[count.index]} from anywhere"
 }
 
 
-
-
-# SG for 8080
+#SG for jenkins port 8080
 
 resource "aws_security_group" "ec2_jenkins_port_8080" {
   name        = var.ec2_jenkins_sg_name
@@ -77,7 +59,6 @@ resource "aws_security_group" "ec2_jenkins_port_8080" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow 8080 port to access jenkins from anywhere"
   }
-
   tags = { Name = "Jenkins SG: 8080" }
 }
 
