2186c34e616cd8b65865b48afe735cdb3a740533
saif25 <saif25@gmail.com>
2025-10-14 21:25:17 +0530

    modify and used fmt

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 module "networking" {
-    source = "./networking"
-    vpc_cidr = var.vpc_cidr
-    vpc_name = var.vpc_name
-    cidr_public_subnet = var.cidr_public_subnet
-    ap_availability_zone = var.ap_availability_zone
-    cidr_private_subnet = var.cidr_private_subnet
+  source               = "./networking"
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = var.vpc_name
+  cidr_public_subnet   = var.cidr_public_subnet
+  ap_availability_zone = var.ap_availability_zone
+  cidr_private_subnet  = var.cidr_private_subnet
 
 }
 
 module "security_groups" {
-    source = "./security-groups"
-    ec2_sg_name = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
-    vpc_id = module.networking.dev_proj_1_vpc_id
-    ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+  source              = "./security-groups"
+  ec2_sg_name         = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  vpc_id              = module.networking.dev_proj_1_vpc_id
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
 }
 
 module "jenkins" {
-    source = "./jenkins"
-    ami_id = var.ec2_ami_id
-    instance_type = "t3.micro"
-    tag_name = "Jenkins:Ubuntu Linux EC2"
-    subnet_id = tolist(module.networking.dev_proj_1_public_subnets)[0]
-    sg_for_jenkins = [module.security_groups.sg_ec2_sg_ssh_http_id, module.security_groups.sg_ec2_jenkins_port_8080]
-    enable_public_ip_address = true
-    user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
-    public_key = var.public_key
+  source                    = "./jenkins"
+  ami_id                    = var.ec2_ami_id
+  instance_type             = "t3.micro"
+  tag_name                  = "Jenkins:Ubuntu 22.04 EC2"
+  subnet_id                 = tolist(module.networking.dev_proj_1_public_subnets)[0]
+  sg_for_jenkins            = [module.security_groups.sg_ec2_sg_ssh_http_id, module.security_groups.sg_ec2_jenkins_port_8080]
+  enable_public_ip_address  = true
+  user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
+  public_key                = var.public_key
 
 }
