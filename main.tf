4d9892f4033ef213cd03319aa95b2e49bc2ec301
saif25 <saif25@gmail.com>
2025-10-15 21:22:19 +0530

    added a module load-balancer-target-group/

--- main.tf
+++ main.tf
@@ -27,3 +27,14 @@ module "jenkins" {
   public_key                = var.public_key
 
 }
+
+module "lb_target_group" {
+  source = "./load-balancer-target-group"
+  lb_target_group_name = "jenkins-lb-target-group"
+  lb_target_group_port = "8080"
+  lb_target_group_protocol = "HTTP"
+  vpc_id = module.networking.dev_proj_1_vpc_id
+  ec2_instance_id = module.jenkins.jenkins_ec2_instance_ip
+
+
+}
