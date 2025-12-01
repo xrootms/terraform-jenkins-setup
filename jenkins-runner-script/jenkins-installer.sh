745b10618caa55ff700d17c4be582c765493ddab
saif25 <saif25@gmail.com>
2025-12-01 12:29:28 +0530

    feat: add more pkg's installation cmds

--- jenkins-runner-script/jenkins-installer.sh
+++ jenkins-runner-script/jenkins-installer.sh
@@ -1,6 +1,12 @@
+
 #!/bin/bash
-sudo apt-get update
-yes | sudo apt install openjdk-17-jre -y
+
+set -ex
+
+sudo apt-get update -y
+sudo apt install openjdk-17-jre-headless -y
+
+# --- JENKINS INSTALLATION STEPS ---
 echo "Waiting for 30 seconds before installing the jenkins package..."
 sleep 30
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
@@ -8,11 +14,62 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
+
+sudo apt-get update -y
 yes | sudo apt-get install jenkins
 sleep 30
-echo "Waiting for 30 seconds before installing the Terraform..."
-wget https://releases.hashicorp.com/terraform/1.13.3/terraform_1.13.3_linux_amd64.zip
-yes | sudo apt-get install unzip
-unzip 'terraform*.zip'
-sudo mv terraform /usr/local/bin/
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# --- DOCKER INSTALLATION STEPS ---
+echo "Waiting for 3 seconds before installing the docker package..."
+sleep 3
+sudo apt  install docker.io -y
+echo "Waiting for 10 seconds before adding User: ubuntu and jenkkins inside docker group..."
+sleep 3
+for u in ubuntu jenkins; do
+  sudo usermod -aG docker $u
+done
+
+# --- TRIVY INSTALLATION STEPS ---
+echo "Installing Trivy in 3 seconds..."
+sleep 3
+sudo apt-get update -y
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
+sleep 3
+
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key \
+  | sudo gpg --dearmor -o /usr/share/keyrings/trivy.gpg > /dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] \
+https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" \
+  | sudo tee /etc/apt/sources.list.d/trivy.list > /dev/null
+
+sleep 3
+sudo apt-get update -y
+sudo apt-get install -y trivy
+
+
+
+# --- KUBECTL INSTALLATION STEPS ---
+echo "Installing Kubectl in 3 seconds..."
+sleep 3
+sudo apt-get update
+sudo snap install kubectl --classic                      #kubectl version --client
+
+
+
+# --- TERRAFORM INSTALLATION STEPS ---
+# echo "Waiting for 10 seconds before installing the Terraform..."
+#sleep 10
+#wget https://releases.hashicorp.com/terraform/1.11.4/terraform_1.11.4_linux_amd64.zip
+# sudo apt-get install -y unzip
+# unzip 'terraform*.zip'
+# sudo mv terraform /usr/local/bin/
+
+# --- CHECKOV INSTALLATION STEPS ---
+# echo "Waiting for 10 seconds before installing the Checkov..."
+#sleep 10
+# sudo apt-get update -y 
+# sudo apt-get install -y python3-pip
+# sudo pip3 install checkov 
